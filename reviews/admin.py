fac5ba2ab845b48c7c0c75f2e58b555e35f0fdb9
alexshary <mail@example.com>
2021-09-17 14:44:48 +0300

    Chapter 4-2

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from reviews.models import Publisher, Contributor, Book, BookContributor, Review
 
+
 # Register your models here.
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ('last_names', 'first_names')
+    list_filter = ('last_names',)
+    search_fields = ('last_names__startswith', 'first_names')
+
 
 admin.site.register(Publisher)
-admin.site.register(Contributor)
+admin.site.register(Contributor, ContributorAdmin)
 admin.site.register(Book)
 admin.site.register(BookContributor)
 admin.site.register(Review)
