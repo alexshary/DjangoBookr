0e99d3161c6edf9be7fe4329ca84f5f4f04a1458
alexshary <mail@example.com>
2021-09-29 16:54:30 +0300

    Chapter 9 activity 9.01

--- bookr/urls.py
+++ bookr/urls.py
@@ -1,7 +1,7 @@
 """bookr URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('accounts/', include(('django.contrib.auth.urls', 'auth'), namespace='accounts')),
     path('admin/', admin.site.urls),
     path('', include('reviews.urls'))
 ]
