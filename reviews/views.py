715a9ec0585cf274a9ad9ebc1fda490ed9ad221f
alexshary <mail@example.com>
2021-09-30 08:17:10 +0300

    Chapter 9-5

--- reviews/views.py
+++ reviews/views.py
@@ -2,6 +2,8 @@ from io import BytesIO
 
 from PIL import Image
 from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test, permission_required, login_required
+from django.core.exceptions import PermissionDenied
 from django.core.files.images import ImageFile
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
@@ -80,7 +82,13 @@ def book_detail(request, pk):
     return render(request, "reviews/book_detail.html", context)
 
 
-@permission_required('edit_publisher')
+# 9.03.2
+def is_staff_user(user):
+    return user.is_staff
+
+
+# @permission_required('edit_publisher')  # 9.03.1
+@user_passes_test(is_staff_user)  # 9.03.2
 def publisher_edit(request, pk=None):
     if pk is not None:
         publisher = get_object_or_404(Publisher, pk=pk)
@@ -104,11 +112,15 @@ def publisher_edit(request, pk=None):
                   {"form": form, "instance": publisher, "model_type": "Publisher"})
 
 
+@login_required  # 9.03.3
 def review_edit(request, book_pk, review_pk=None):
     book = get_object_or_404(Book, pk=book_pk)
 
     if review_pk is not None:
         review = get_object_or_404(Review, book_id=book_pk, pk=review_pk)
+        user = request.user
+        if not user.is_staff and review.creator.id != user.id:
+            raise PermissionDenied
     else:
         review = None
 
@@ -139,6 +151,7 @@ def review_edit(request, book_pk, review_pk=None):
                    })
 
 
+@login_required  # 9.03.3
 def book_media(request, pk):
     book = get_object_or_404(Book, pk=pk)
 
